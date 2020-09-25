3c1d1b7272e43e32426685f2f377ba4cc745b35b
Sherfra <sherlyfratista11@gmail.com>
2020-09-25 10:00:34 +0700

    Aplikasi Penjadwalan Lab Praktikum

---
+++ app/Http/Controllers/MatkulController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Jam;
+use App\Hari;
+use App\Ruang;
+use App\Matkul;
+class MatkulController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $matkul =  DB::table('matkul')->get();
+        return view('laboran.matkul', ['matkul'=>$matkul]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+        return view('laboran.input_matkul');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $matkul = Matkul::create([
+            'kode_mk'=> $request->kode_mk,
+            'nama_mk'=> $request->nama_mk,
+            'sks'=> $request->sks,
+            'semester'=> $request->semester,
+            'tahun'=> $request->tahun,
+             ]);
+      
+        return redirect ('laboran/matkul')->with('sukses','Data Berhasil Diinput!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Matkul $matkul)
+    {
+        return view('laboran.edit_matkul', compact('matkul'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Matkul $matkul)
+    {
+        return view('laboran.edit_matkul', compact('matkul'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  Matkul $matkul)
+    {
+        Matkul::where('id',$matkul->id)
+        ->update([
+            'kode_mk'=> $request->kode_mk,
+            'nama_mk'=> $request->nama_mk,
+            'sks'=> $request->sks,
+            'semester'=> $request->semester,
+            'tahun'=> $request->tahun,
+        ]);
+        return redirect ('laboran/matkul')->with('sukses','Data Berhasil diUpdate!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Matkul $matkul)
+    {
+        Matkul::destroy($matkul->id);
+        return redirect ('laboran/matkul')->with('sukses','Data Berhasil Dihapus!');
+    }
+}
